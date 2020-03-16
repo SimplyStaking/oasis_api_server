0cc21d9a13f0ddcdfcae3724e2e3a58352670266
vvol <vitaly.volozhinov.16@um.edu.mt>
2020-03-16 14:15:08 +0100

    General Handler Tests Completed

---
+++ src/handlers/general_handler_test.go
@@ -0,0 +1,53 @@
+package handlers_test
+
+import (
+	"testing"
+	"strings"
+	"net/http"
+	"net/http/httptest"
+
+	hdl "github.com/SimplyVC/oasis_api_server/src/handlers"
+	// responses "github.com/SimplyVC/oasis_api_server/src/responses"
+)
+
+func Test_Pong(t *testing.T){
+	req, _ := http.NewRequest("GET", "/api/ping", nil)
+	q := req.URL.Query()
+	req.URL.RawQuery = q.Encode()
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(hdl.Pong)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"result":"pong"}`
+
+	if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(expected) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func Test_GetConnections(t *testing.T){
+	req, _ := http.NewRequest("GET", "/api/getConnectionsList", nil)
+	q := req.URL.Query()
+	req.URL.RawQuery = q.Encode()
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(hdl.GetConnections)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"result":["unix:/home/vvol/serverdir/nodes/internal.sock","unix:/media/nuclearvodka/_backup/Oasis_node/serverdir/node/internal.sock","unix:/media/nuclearvodka/_backup/Oasis_node/serverdir/node/internal.sock"]}`
+
+	if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(expected) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
